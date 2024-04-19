406f4a241c50d2e78d378e998daa7c0ecc703b98
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-04-19 19:29:02 -0300

    Criado fabrica de aplicativo.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,26 @@
 import os
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    @app.route('/')
+def create_app(test_config=None):
+    # Cria e configura o aplicativo
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KET='dev', DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),)
+
+    if test_config is None:
+        # Carrega a configuração da instância, se existir, quando não estiver testando
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Carrega a configuração do teste se aprovado
+        app.config.from_mapping(test_config)
+
+    # Garantir que a pasta da instancia existe
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # Uma pagina simples que diz hello!
+    @app.route('/hello')
     def hello():
-        return 'Hello, World Im learn flask!'
+        return 'Hello, World! Im learn flask!'
     return app
