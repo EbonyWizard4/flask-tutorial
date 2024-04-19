a3c5cdf6a26aa439aabed3b5de38e16c299895d9
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-04-19 20:38:18 -0300

    config database sqlite structure

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,4 +23,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World! Im learn flask!'
+    
+    from . import db
+    db.init_app(app)
+
     return app
