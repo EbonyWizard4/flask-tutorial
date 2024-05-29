036ba47e9caf1ca2dfa756e008f12d749889f3b1
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-05-29 20:34:17 -0300

    escrito codigo de teste de autenticação

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,3 +47,20 @@ def runner(app):
         Permite executar os comando registrados no app
     '''
     return app.test_cli_runner()
+
+class AuthActions(object):
+    def __init__(self, client) -> None:
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username':username, 'password':password}
+        )
+    
+    def logout(self):
+        return self._client.get('/auth/logout')
+    
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
